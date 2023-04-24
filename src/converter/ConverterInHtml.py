6407f17d94c0d8e4c5dfebb7e7fa4562d67887ae
RozumLiza <85706673+RozumLiza@users.noreply.github.com>
2023-04-25 01:10:44 +0300

    Update ConverterInHtml.py

--- src/converter/ConverterInHtml.py
+++ src/converter/ConverterInHtml.py
@@ -1,15 +1,14 @@
 import markdown
 import os
 
-def ConvHtml(file):
+def ConvHtml(full_path, new_path): 
     # Читая sample.md и сохранение его содержимого в переменной markdown_string
-    with open(file, 'r') as f:
+    with open(full_path, 'r') as f:
         markdown_string = f.read()
 
     # Преобразование markdown (markdown_string) в HTML (html_string) с использованием метода markdown из пакета markdown.
     html_string = markdown.markdown(markdown_string)
 
     # Создание sample.html файл и запись в него HTML (html_string).
-    file2 = os.path.splitext(file)[0]+".html"
-    with open(file2, 'w') as f:
+    with open(new_path, 'w') as f:
         f.write(html_string)
