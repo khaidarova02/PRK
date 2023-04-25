1e58ceb9c3a402e319a72d9be2ef843e1300e7af
RozumLiza <85706673+RozumLiza@users.noreply.github.com>
2023-04-25 13:38:26 +0300

    Добавлены комментарии

--- src/converter/ConverterInHtml.py
+++ src/converter/ConverterInHtml.py
@@ -2,13 +2,13 @@ import markdown
 import os
 
 def ConvHtml(full_path, new_path): 
-    # Читая sample.md и сохранение его содержимого в переменной markdown_string
+    # Чтение файла и сохранение его содержимого в переменной markdown_string
     with open(full_path, 'r') as f:
         markdown_string = f.read()
 
     # Преобразование markdown (markdown_string) в HTML (html_string) с использованием метода markdown из пакета markdown.
     html_string = markdown.markdown(markdown_string)
 
-    # Создание sample.html файл и запись в него HTML (html_string).
+    # Создание файла и запись в него HTML (html_string).
     with open(new_path, 'w') as f:
         f.write(html_string)
