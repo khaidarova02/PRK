205de6059c872c55cd39c0980581987f48ff20f1
RozumLiza <85706673+RozumLiza@users.noreply.github.com>
2023-05-03 12:55:32 +0300

    Обновлены комментарии

--- src/converter/ConverterInHtml.py
+++ src/converter/ConverterInHtml.py
@@ -2,16 +2,17 @@ import markdown
 import os
 
 def ConvHtml(full_path, new_path): 
+    # проверка на права доступа
     if not os.access(full_path, os.R_OK): 
         print("Недостаточно прав для чтения файла")
         return
-    # Читая sample.md и сохранение его содержимого в переменной markdown_string
+    # Чтение файла и сохранение его содержимого в переменной markdown_string
     with open(full_path, 'r') as f:
         markdown_string = f.read()
 
     # Преобразование markdown (markdown_string) в HTML (html_string) с использованием метода markdown из пакета markdown.
     html_string = markdown.markdown(markdown_string)
 
-    # Создание sample.html файл и запись в него HTML (html_string).
+     # Создание файла и запись в него HTML (html_string).
     with open(new_path, 'w') as f:
         f.write(html_string)
