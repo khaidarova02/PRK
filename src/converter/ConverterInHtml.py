060888159cc0fdd616132e353466a596ac820c54
RozumLiza <85706673+RozumLiza@users.noreply.github.com>
2023-05-03 14:24:50 +0300

    Изменено с требованиями pylint

--- src/converter/ConverterInHtml.py
+++ src/converter/ConverterInHtml.py
@@ -1,7 +1,8 @@
 import markdown
 import os
 
-def ConvHtml(full_path, new_path): 
+
+def convHtml(full_path, new_path): 
     # проверка на права доступа
     if not os.access(full_path, os.R_OK): 
         print("Недостаточно прав для чтения файла")
@@ -11,10 +12,10 @@ def ConvHtml(full_path, new_path):
     with open(full_path, 'r') as f:
         markdown_string = f.read()
 
-    # Преобразование markdown (markdown_string) в HTML (html_string) с использованием метода markdown из пакета markdown.
+    # Преобразование markdown в HTML 
+    # с использованием метода markdown из пакета markdown.
     html_string = markdown.markdown(markdown_string)
 
-     # Создание файла и запись в него HTML (html_string).
-
+    # Создание файла и запись в него HTML (html_string).
     with open(new_path, 'w') as f:
         f.write(html_string)
