1412eb5847a6be38fa0b2c1cad90816ac8871e7c
SkurtenkoShadi <86601920+SkurtenkoShadi@users.noreply.github.com>
2023-05-22 03:07:41 +0300

    Update xml_to_markdown.py

--- src/converter/xml_to_markdown.py
+++ src/converter/xml_to_markdown.py
@@ -1,21 +1,20 @@
+import markdown
 import io
-import xmltodict
 
 def Conv_MD(full_path, new_path):
     try:
         try:
             # Открываем XML-файл
-            with io.open(full_path,'r', encoding="utf-8") as xml_file: # Открываем XML-файл
-        # Парсим XML-файл в словарь
-                data_dict = xmltodict.parse(xml_file.read())
+            with io.open(full_path, 'r', encoding="utf-8") as f:
+                # Считываем XML-файл 
+                input_string = f.read()
         except PermissionError:
             print ("Недостаточно прав для чтения файла")
         else:
-            with io.open(new_path, 'w', encoding="utf8") as md_file:
-                for key in data_dict:
-                    md_file.write(f"# {key}\n\n") # Заголовок наименования элемента
-                    for subkey, subvalue in data_dict[key].items():
-                        md_file.write(f"**{subkey}:** {subvalue}\n") #Строка с наименованием и значением
-                    md_file.write("\n") # Добавляем пустую строку
+            # Конвертируем в markdown
+            output_string = markdown.markdown(input_string)
+            #Записываем в файл
+            with io.open(new_path, 'w', encoding="utf-8") as f:
+                f.write(output_string)
     except Exception:
         print (f'File {full_path} cannot be converted')
