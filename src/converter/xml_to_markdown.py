3526a91ce79dcb3042700f892c2bac4e0c25415e
SkurtenkoShadi <86601920+SkurtenkoShadi@users.noreply.github.com>
2023-05-24 00:31:24 +0300

    Update xml_to_markdown.py

--- src/converter/xml_to_markdown.py
+++ src/converter/xml_to_markdown.py
@@ -12,6 +12,15 @@ def Conv_MD(full_path, new_path):
         except PermissionError:
             print ("Недостаточно прав для чтения файла")
         else:
+            lines = input_string.splitlines()
+
+            # Удаляем первую строку, если она начинается с "xml version"
+            if lines[0].startswith('<?xml version'):
+                if lines[1].startswith('<?xml'):
+                    del lines[1]
+                del lines[0]
+            # Объединяем оставшиеся строки в одну строку с символом переноса строки
+            input_string = '\n'.join(lines)
             # Конвертируем в markdown
             output_string = markdown.markdown(input_string)
             output_string = markdownify.markdownify(output_string)
